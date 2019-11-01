ad3701975f872b597d675fccc63dfefeb12df711
VMoraes26 <vimoraescarvalho@gmail.com>
2019-11-01 01:13:36 -0300

    Criação da métricas

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { calcularFPb } from './metricas';
 
 export default function App() {
-  const [tabelas, setTabelas] = useState({})
+  const [tabelas, setTabelas] = useState({ a: '5', b: '6', c: '12', d: '18', e: '7' })
   const [tabelaNome, setTabelaNome] = useState('')
   const [tabelaQtdCampos, setTabelaQtdCampos] = useState('')
   const [conteudo, setConteudo] = useState([])
+  const [FPb, setFPb] = useState(0)
+  const [linhas, setLinhas] = useState('20')
+  const [produtividade, setProdutividade] = useState('2500')
+  const [valorHora, setValorHora] = useState('15')
 
   useEffect(() => {
     renderTable()
@@ -30,7 +35,7 @@ export default function App() {
             editable={false}
           />
           <TouchableOpacity
-            style={{ width: '20%', alignItems: 'center' }}
+            style={{ width: '20%', alignItems: 'center', backgroundColor: 'red', justifyContent: 'center', }}
             onPress={() => removeTable(tabela)}
           >
             <Text>X</Text>
@@ -51,8 +56,13 @@ export default function App() {
 
   function removeTable(table) {
     var tables = tabelas
+    console.log(table)
     delete tables[table]
     setTabelas({ ...tables })
+  }
+
+  async function calcular() {
+    await setFPb(await calcularFPb(tabelas, linhas, produtividade, valorHora))
   }
 
   return (
@@ -63,6 +73,8 @@ export default function App() {
         <TextInput
           style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '40%' }}
           placeholder="Nome da tabela"
+          autoCompleteType="off"
+          autoCorrect={false}
           value={tabelaNome}
           onChangeText={(text) => setTabelaNome(text)}
         />
@@ -77,13 +89,47 @@ export default function App() {
         />
 
         <TouchableOpacity
-          style={{ width: '20%', alignItems: 'center' }}
+          style={{ width: '20%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'green' }}
           onPress={addTable}
         >
           <Text>+</Text>
         </TouchableOpacity>
 
       </View>
+
+
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '40%' }}
+        keyboardType='numeric'
+        placeholder="Linhas de codigo por linguagem"
+        value={linhas}
+        onChangeText={(text) => setLinhas(text)}
+      />
+
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '40%' }}
+        keyboardType='numeric'
+        placeholder="Produtividade(Kloc/Loc)"
+        value={produtividade}
+        onChangeText={(text) => setProdutividade(text)}
+      />
+
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '40%' }}
+        keyboardType='numeric'
+        placeholder="Valor da hora"
+        value={valorHora}
+        onChangeText={(text) => setValorHora(text)}
+      />
+
+      <TouchableOpacity
+        style={{ width: '100%', alignItems: 'center', height: 40, backgroundColor: 'orange', justifyContent: 'center' }}
+        onPress={calcular}
+      >
+        <Text>Calcular</Text>
+      </TouchableOpacity>
+
+      <Text>{FPb}</Text>
 
     </KeyboardAvoidingView>
   );
---
+++ metricas.js
@@ -0,0 +1,343 @@
+export function calcularFPb(tabelas, linhas, produtividade, valorHora) {
+
+    var FPb =
+        entrada(tabelas) +
+        saida(tabelas) +
+        consulta(tabelas) +
+        arquivo(tabelas) +
+        interfaceOk(tabelas)
+
+    console.log('FPb ' + FPb)
+
+    var FPr = (FPb * 1.35).toFixed(0)
+    console.log('FPr ' + FPr)
+
+    var linhasDeCodigo = parseInt(linhas)
+    console.log('Linguagem Java ' + linhasDeCodigo)
+
+    var produtividadeSistema = parseInt(produtividade)
+    console.log('Tipo de sistema ' + produtividadeSistema)
+
+    var valorHora = parseInt(valorHora)
+    console.log('Valor da hora ' + valorHora)
+
+    var resultado = (FPr * linhasDeCodigo) / produtividadeSistema
+    let [inteiro, decimal] = String(resultado).split('.')
+    if (decimal) {
+        resultado = parseFloat(inteiro + '.' + decimal.substring(0, 2))
+    }
+
+    console.log(resultado)
+
+    custo(resultado, valorHora)
+
+    prazo(resultado)
+}
+
+function entrada(tabelas) {
+    //Náo utiliza tabela geral
+    delete tabelas.geral
+
+    let primeira = 0
+    let segunda = 0
+    let terceira = 0
+
+    let simples = 0
+    let medio = 0
+    let complexo = 0
+
+    for (tabela in tabelas) {
+        let valor = parseInt(tabelas[tabela])
+
+        if (valor >= 1 && valor <= 4) {
+            primeira++
+        } else if (valor >= 5 && valor <= 15) {
+            segunda++
+        } else if (valor >= 16) {
+            terceira++
+        }
+    }
+
+    if (primeira == 0 || primeira == 1) {
+        simples += primeira
+    } else if (primeira == 2) {
+        simples += primeira
+    } else if (primeira >= 3) {
+        medio += primeira
+    }
+
+    if (segunda == 0 || segunda == 1) {
+        simples += segunda
+    } else if (segunda == 2) {
+        medio += segunda
+    } else if (segunda >= 3) {
+        complexo += segunda
+    }
+
+    if (terceira == 0 || terceira == 1) {
+        medio += terceira
+    } else if (terceira == 2) {
+        complexo += terceira
+    } else if (terceira >= 3) {
+        complexo += terceira
+    }
+
+    console.log(simples, medio, complexo)
+    console.log(simples * 3, medio * 4, complexo * 6)
+    return (simples * 3 + medio * 4 + complexo * 6)
+}
+
+function saida(tabelas) {
+    //Utiliza tabela geral
+    let total = Object.values(tabelas).reduce((a, b) => parseInt(a) + parseInt(b))
+    tabelas.geral = total
+
+    let primeira = 0
+    let segunda = 0
+    let terceira = 0
+
+    let simples = 0
+    let medio = 0
+    let complexo = 0
+
+    for (tabela in tabelas) {
+        let valor = parseInt(tabelas[tabela])
+
+        if (valor >= 1 && valor <= 5) {
+            primeira++
+        } else if (valor >= 6 && valor <= 19) {
+            segunda++
+        } else if (valor >= 20) {
+            terceira++
+        }
+    }
+
+    if (primeira == 0 || primeira == 1) {
+        simples += primeira
+    } else if (primeira == 2 || primeira == 3) {
+        simples += primeira
+    } else if (primeira >= 4) {
+        medio += primeira
+    }
+
+    if (segunda == 0 || segunda == 1) {
+        simples += segunda
+    } else if (segunda == 2 || segunda == 3) {
+        medio += segunda
+    } else if (segunda >= 4) {
+        complexo += segunda
+    }
+
+    if (terceira == 0 || terceira == 1) {
+        medio += terceira
+    } else if (terceira == 2 || terceira == 3) {
+        complexo += terceira
+    } else if (terceira >= 4) {
+        complexo += terceira
+    }
+
+    delete tabelas.geral
+
+    console.log(simples, medio, complexo)
+    console.log(simples * 4, medio * 5, complexo * 7)
+    return (simples * 4 + medio * 5 + complexo * 7)
+}
+
+function consulta(tabelas) {
+    //Utiliza tabela geral
+    let total = Object.values(tabelas).reduce((a, b) => parseInt(a) + parseInt(b))
+    tabelas.geral = total
+
+    let primeira = 0
+    let segunda = 0
+    let terceira = 0
+
+    let simples = 0
+    let medio = 0
+    let complexo = 0
+
+    for (tabela in tabelas) {
+        let valor = parseInt(tabelas[tabela])
+
+        if (valor >= 1 && valor <= 4) {
+            primeira++
+        } else if (valor >= 5 && valor <= 15) {
+            segunda++
+        } else if (valor >= 16) {
+            terceira++
+        }
+    }
+
+    if (primeira == 0 || primeira == 1) {
+        simples += primeira
+    } else if (primeira == 2) {
+        simples += primeira
+    } else if (primeira >= 3) {
+        medio += primeira
+    }
+
+    if (segunda == 0 || segunda == 1) {
+        simples += segunda
+    } else if (segunda == 2) {
+        medio += segunda
+    } else if (segunda >= 3) {
+        complexo += segunda
+    }
+
+    if (terceira == 0 || terceira == 1) {
+        medio += terceira
+    } else if (terceira == 2) {
+        complexo += terceira
+    } else if (terceira >= 3) {
+        complexo += terceira
+    }
+
+    delete tabelas.geral
+
+    console.log(simples, medio, complexo)
+    console.log(simples * 3, medio * 4, complexo * 6)
+    return (simples * 3 + medio * 4 + complexo * 6)
+}
+
+function arquivo(tabelas) {
+    //Náo utiliza tabela geral
+    delete tabelas.geral;
+
+    let primeira = 0
+    let segunda = 0
+    let terceira = 0
+
+    let simples = 0
+    let medio = 0
+    let complexo = 0
+
+    for (tabela in tabelas) {
+        let valor = parseInt(tabelas[tabela])
+
+        if (valor >= 1 && valor <= 19) {
+            primeira++
+        } else if (valor >= 20 && valor <= 50) {
+            segunda++
+        } else if (valor >= 51) {
+            terceira++
+        }
+    }
+
+    if (primeira == 1) {
+        simples += primeira
+    } else if (primeira >= 2 && primeira <= 5) {
+        simples += primeira
+    } else if (primeira >= 6) {
+        medio += primeira
+    }
+
+    if (segunda == 1) {
+        simples += segunda
+    } else if (segunda >= 2 && segunda <= 5) {
+        medio += segunda
+    } else if (segunda >= 6) {
+        complexo += segunda
+    }
+
+    if (terceira == 1) {
+        medio += terceira
+    } else if (terceira >= 2 && terceira <= 5) {
+        complexo += terceira
+    } else if (terceira >= 6) {
+        complexo += terceira
+    }
+
+    delete tabelas.geral
+
+    console.log(simples, medio, complexo)
+    console.log(simples * 7, medio * 10, complexo * 15)
+    return (simples * 7 + medio * 10 + complexo * 15)
+}
+
+function interfaceOk(tabelas) {
+    //Utiliza tabela geral
+    let total = Object.values(tabelas).reduce((a, b) => parseInt(a) + parseInt(b))
+    tabelas.geral = total
+
+
+    let primeira = 0
+    let segunda = 0
+    let terceira = 0
+
+    let simples = 0
+    let medio = 0
+    let complexo = 0
+
+    for (tabela in tabelas) {
+        let valor = parseInt(tabelas[tabela])
+
+        if (valor >= 1 && valor <= 19) {
+            primeira++
+        } else if (valor >= 20 && valor <= 50) {
+            segunda++
+        } else if (valor >= 51) {
+            terceira++
+        }
+    }
+
+    if (primeira == 1) {
+        simples += primeira
+    } else if (primeira >= 2 && primeira <= 5) {
+        simples += primeira
+    } else if (primeira >= 6) {
+        medio += primeira
+    }
+
+    if (segunda == 1) {
+        simples += segunda
+    } else if (segunda >= 2 && segunda <= 5) {
+        medio += segunda
+    } else if (segunda >= 6) {
+        complexo += segunda
+    }
+
+    if (terceira == 1) {
+        medio += terceira
+    } else if (terceira >= 2 && terceira <= 5) {
+        complexo += terceira
+    } else if (terceira >= 6) {
+        complexo += terceira
+    }
+
+    delete tabelas.geral
+
+    console.log(simples, medio, complexo)
+    console.log(simples * 5, medio * 7, complexo * 10)
+    return (simples * 5 + medio * 7 + complexo * 10)
+}
+
+function custo(resultado, valorHora) {
+    var custo = resultado * 22 * 6 * valorHora
+    console.log(custo.toFixed(2))
+}
+
+function prazo(resultado) {
+    var prazoMeses = 0
+    var prazoDias = 0
+    var prazoHoras = 0
+    var prazoMinutos = 0
+
+    let [meses, dias] = String(resultado).split('.')
+    prazoMeses = parseInt(meses)
+    if (dias) {
+        prazoDias = 22 * parseFloat('0.' + dias.substring(0, 2))
+        let horas = String(prazoDias).split('.')[1]
+        if (horas) {
+            prazoHoras = 6 * parseFloat('0.' + horas.substring(0, 2))
+            let minutos = String(prazoHoras).split('.')[1]
+            if (minutos) {
+                prazoMinutos = 60 * parseFloat('0.' + minutos.substring(0, 2))
+            }
+        }
+    }
+
+    console.log(String(prazoMeses).split('.')[0] + " meses")
+    console.log(String(prazoDias).split('.')[0] + " dias")
+    console.log(String(prazoHoras).split('.')[0] + " horas")
+    console.log(String(prazoMinutos).split('.')[0] + " minutos")
+}
